55ffdae124ea382fd2a76149772ac9c7d7384eac
Erin Dustin <erincdustin@gmail.com>
2019-08-13 14:25:53 -0400

    added authRouter to app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const authRouter = require('./auth/auth-router')
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 
+app.use('/api/auth', authRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')

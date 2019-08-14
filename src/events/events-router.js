9d5289d8a998df9abe74d937868684532a497a88
Jonathan Moore <jmjonemoore@gmail.com>
2019-08-14 11:39:10 -0500

    Building the tests

---
+++ src/events/events-router.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+const EventsService = require('./events-service')
+const { requireAuth } = require('../middleware/jwt-auth')
+
+const eventsRouter = express.Router()
+const jsonParser = express.json()
+
+eventsRouter
+  .use(requireAuth)
+
+eventsRouter
+  .get('/', async (req, res, next) => {
+    try {
+      const events = await EventsService.getEvents(
+        req.app.get('db'),
+        req.user.id
+      )
+
+      res.json(
+        events
+      )
+      next()
+    }
+    catch (error) {
+      next(error)
+    }
+  })
+
+  module.exports = eventsRouter

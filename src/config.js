4190466488afa72925eed5d3a501be83269bad76
Jonathan Moore <jmjonemoore@gmail.com>
2019-08-16 15:23:01 -0500

    Preparing config.js for deployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   AUTHENTIC_JOBS_API_TOKEN: process.env.AUTHENTIC_JOBS_API_TOKEN || 'none',
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/jobreel',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/jobreel',
+  // DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/jobreel',
   TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/jobreel-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'

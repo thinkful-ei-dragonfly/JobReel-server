6ce8c17de9b5232a9fb1b346b3560977d23505d5
cfiander <cfiander90@gmail.com>
2019-08-23 14:04:33 -0700

    route changes

--- src/eventbrite/eventbrite-router.js
+++ src/eventbrite/eventbrite-router.js
@@ -7,20 +7,12 @@ const jsonBodyParser = express.json()
 eventbriteRouter
   .route(`/`)
     .get((req, res, next) => { 
-      res.send({url: `https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=I6MVEHHYVS3LD42Z46&redirect_uri=https://localhost:8000/api/eventbrite`})
+      res.send({url: `https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=I6MVEHHYVS3LD42Z46&redirect_uri=https://stormy-beyond-18995.herokuapp.com/api/eventbrite`})
     })
-    // .post(jsonBodyParser, (req, res, next) => {
-    //     // const {domain, company, seniority, department} = req.body.search
-    //     unirest.get(`https://secure.meetup.com/oauth2/authorize
-    //     ?client_id=67tbmv2gfetercp9rke84h8ale
-    //     &response_type=token
-    //     &redirect_uri=http://localhost:3000`)
-    //     .end(function (result) {
-    //         if (result.error) throw new Error(result.error)
-    //         console.log(result)
-    //         res.status(200).send(result.body);
-    //     })
-    // })
+    .post(jsonBodyParser, (req, res, next) => {
+        // const {domain, company, seniority, department} = req.body.search
+        unirest.get()
+    })
 
 
 module.exports = eventbriteRouter

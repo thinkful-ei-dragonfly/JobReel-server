13f60fc1574cb6f838d5621225a6c0ce09d3dc56
Jonathan Moore <jonathan.moore6247@gmail.com>
2019-08-30 13:30:28 -0400

    Modified config

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ module.exports = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
   AUTHENTIC_JOBS_API_TOKEN: process.env.AUTHENTIC_JOBS_API_TOKEN,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/jobreel',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/jobreel-test',
+  NODE_ENV: process.env.NODE_ENV,
+  DB_URL: process.env.DATABASE_URL,
+  TEST_DB_URL: process.env.TEST_DB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY
 }
